da124ed69e3d6f62598ec6cd34e291f7f8e70f9e
Doyun Yu <doyun0109@icloud.com>
2024-09-19 02:04:06 +0900

    create camp-list

---
+++ src/components/common/CampList.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import styles from '@/components/common/CampList.module.scss';
+import Camp from "@/lib/types/Camp";
+import {useState} from "react";
+import Department from "@/lib/types/Department";
+import CampCard from "@/components/common/CampCard";
+import { useRouter } from 'next/navigation'
+
+
+interface Props {
+    campList: Camp[]
+}
+
+const CampList = ({campList}: Props) => {
+    const router = useRouter();
+    const [department, setDepartment] = useState<Department | null>(null);
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.buttonContainer}>
+                <button
+                    className={styles.filterButton}
+                    onClick={() => setDepartment(null)}
+                    data-selected={department === null}>
+                    전체
+                </button>
+                <button
+                    className={styles.filterButton}
+                    onClick={() => setDepartment('Software')}
+                    data-selected={department === 'Software'}>
+                    소프트웨어과
+                </button>
+                <button
+                    className={styles.filterButton}
+                    onClick={() => setDepartment('Security')}
+                    data-selected={department === 'Security'}>
+                    정보보호과
+                </button>
+            </div>
+
+            <div className={styles.campCardContainer}>
+                {campList.filter((camp) =>
+                    department ? camp.department === department : true
+                ).map((camp, index) => (
+                    <CampCard key={index} camp={camp} onClick={() => router.push(`/camp/${camp.clubId}`)}/>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default CampList;
