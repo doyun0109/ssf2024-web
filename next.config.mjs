a11e6ff65d7f2167e0cfa9f9d969f10bd1a95c1d
Doyun Yu <doyun0109@icloud.com>
2024-09-19 02:40:03 +0900

    edit image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["firebasestorage.googleapis.com"], // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 된다.
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "firebasestorage.googleapis.com",
+                pathname: '**',
+            }
+        ]
     },
 };
 
