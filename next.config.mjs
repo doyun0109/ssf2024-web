6503d50bb58c51bf1a4113faeca9032b1d44b41b
Doyun Yu <doyun0109@icloud.com>
2024-09-19 02:02:56 +0900

    image setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["firebasestorage.googleapis.com"], // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 된다.
+    },
+};
 
 export default nextConfig;
