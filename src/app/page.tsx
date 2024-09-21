523aad98760c63c74f84b149dc85963aefa25457
Doyun Yu <doyun0109@icloud.com>
2024-09-21 17:59:00 +0900

    root page SSG -> ISR -revalidate 10 minutes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import CampSection from "@/components/section/CampSection";
 import ScheduleSection from "@/components/section/ScheduleSection";
 import QuestionSection from "@/components/section/QuestionSection";
 
+export const revalidate = 600; // 10 minutes
+
+
 const Home = () => {
   return (
       <>
