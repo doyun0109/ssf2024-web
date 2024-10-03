dba3ae28d4c3012b1421feb1a20b03bac6166abf
Doyun Yu <doyun0109@icloud.com>
2024-10-04 05:35:04 +0900

    update onboarding

--- src/components/section/OnBoardingSection.tsx
+++ src/components/section/OnBoardingSection.tsx
@@ -1,9 +1,30 @@
-
 import styles from './OnBoardingSection.module.scss';
+import Image from "next/image";
 
 const OnBoardingSection = () => {
     return (
         <div className={styles.container}>
+            <Image
+                className={styles.background}
+                src={'/assets/image/onboarding-background.svg'}
+                alt={'background'}
+                fill
+                priority
+            />
+            <div className={styles.contentContainer}>
+                <div className={styles.sunrinLogoContainer}>
+                    <Image src={'/assets/icon/sunrin-logo.svg'} alt={'sunrin-logo'} priority fill/>
+                </div>
+                <div className={styles.ssfLogoContainer}>
+                    <Image src={'/assets/icon/ssf-logo.svg'} alt={'ssf-logo'} priority fill/>
+                </div>
+                <p className={styles.typography}>
+                    2024 Software Share Festival
+                </p>
+            </div>
+
+            <div className={styles.overlay}/>
+
             <div className={styles.buttonContainer}>
                 <button className={styles.applyButton}>
                     지원하기
